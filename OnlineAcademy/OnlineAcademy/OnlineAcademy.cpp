6a9cd0ac0ca2820ca82606784fa3c9db18d03144
YZIlcheva21 <YZIlcheva21@codingburgas.bg>
2024-04-14 17:48:05 +0300

    Add two submenus

--- OnlineAcademy/OnlineAcademy/OnlineAcademy.cpp
+++ OnlineAcademy/OnlineAcademy/OnlineAcademy.cpp
@@ -78,7 +78,7 @@ void color(int color)
 
 void printMenuOptions(std::vector<std::string> menuOptions, int selectedOption)
 {
-    for (int i = 0; i < 3; i++)
+    for (size_t i = 0; i < menuOptions.size(); i++)
     {
         // Print arrow before selected option
         if (i == selectedOption)
@@ -96,16 +96,166 @@ void printMenuOptions(std::vector<std::string> menuOptions, int selectedOption)
     }
 }
 
+void printSubMenuOptions(std::vector<std::string> subMenuOptions, int selectedOption)
+{
+    for (size_t i = 0; i < subMenuOptions.size(); ++i)
+    {
+        // Print arrow before selected option
+        if (i == selectedOption)
+        {
+            outputPosition(1, i + 1);
+            std::cout << "-> " << subMenuOptions[i];
+        }
+
+        // Print space before not selected option
+        else
+        {
+            outputPosition(1, i + 1);
+            std::cout << "   " << subMenuOptions[i];
+        }
+    }
+}
+
+bool subMenuStartTest(std::vector<std::string> subMenuStartTestOptions, std::vector<std::string> menuOptions)
+{
+    int selectedOption = 0;
+    char pressedKey = ' ';
+
+    system("CLS"); // Clear the screen
+
+    printSubMenuOptions(subMenuStartTestOptions, selectedOption);
+
+    while (true)
+    {
+        pressedKey = _getch();
+
+        // Moving up through the submenu
+        if (selectedOption != 0 && pressedKey == (char)72)
+        {
+            selectedOption--;
+        }
+
+        // Moving down through the submenu
+        if (selectedOption != subMenuStartTestOptions.size() - 1 && pressedKey == (char)80)
+        {
+            selectedOption++;
+        }
+
+        system("CLS"); // Clear the screen
+
+        printSubMenuOptions(subMenuStartTestOptions, selectedOption);
+
+        // Choosing submenu option
+        if (pressedKey == '\r')
+        {
+            switch (selectedOption)
+            {
+                // Option 1
+            case 0:
+                system("CLS");
+                std::cout << "Option 1 selected." << std::endl;
+                break;
+
+                // Option 2
+            case 1:
+                system("CLS");
+                std::cout << "Option 2 selected." << std::endl;
+                break;
+
+                // Back
+            case 2:
+                system("CLS");
+                return true; // Return to main menu
+
+            default:
+                break;
+            }
+        }
+    }
+}
+
+bool subMenuGrades(std::vector<std::string> subMenuGradesOptions, std::vector<std::string> menuOptions)
+{
+    int selectedOption = 0;
+    char pressedKey = ' ';
+
+    system("CLS"); // Clear the screen
+
+    printSubMenuOptions(subMenuGradesOptions, selectedOption);
+
+    while (true)
+    {
+        pressedKey = _getch();
+
+        // Moving up through the submenu
+        if (selectedOption != 0 && pressedKey == (char)72)
+        {
+            selectedOption--;
+        }
+
+        // Moving down through the submenu
+        if (selectedOption != subMenuGradesOptions.size() - 1 && pressedKey == (char)80)
+        {
+            selectedOption++;
+        }
+
+        system("CLS"); // Clear the screen
+
+        printSubMenuOptions(subMenuGradesOptions, selectedOption);
+
+        // Choosing submenu option
+        if (pressedKey == '\r')
+        {
+            switch (selectedOption)
+            {
+                // Option 1
+            case 0:
+                system("CLS");
+                std::cout << "Displaying highest grade." << std::endl;
+                // Implement displaying highest grade functionality
+                break;
+
+                // Option 2
+            case 1:
+                system("CLS");
+                std::cout << "Displaying students who passed the test." << std::endl;
+                // Implement displaying students who passed the test functionality
+                break;
+
+                // Option 3
+            case 2:
+                system("CLS");
+                std::cout << "Displaying students who don't pass the test." << std::endl;
+                // Implement displaying students who don't pass the test functionality
+                break;
+
+                // Back
+            case 3:
+                system("CLS");
+                return true; // Return to main menu
+
+            default:
+                break;
+            }
+        }
+    }
+}
+
+
 void menu(std::vector<std::string> menuOptions)
 {
     int selectedOption = 0; // By default when you open the app you are hovering at the first menu option
     char pressedKey = ' ';
     bool exitStatement = true;
-
-    printMenuOptions(menuOptions, selectedOption);
+    bool displayMenu = true; // Flag to control whether to display the main menu
 
     while (exitStatement)
     {
+        if (displayMenu) {
+            system("CLS"); // Clear the screen
+            printMenuOptions(menuOptions, selectedOption);
+        }
+
         pressedKey = _getch();
 
         // Moving up through the menu
@@ -115,32 +265,60 @@ void menu(std::vector<std::string> menuOptions)
         }
 
         // Moving down through the menu
-        if (selectedOption != 2 && pressedKey == (char)80)
+        if (selectedOption != menuOptions.size() - 1 && pressedKey == (char)80)
         {
             selectedOption++;
         }
 
-        printMenuOptions(menuOptions, selectedOption);
+        if (displayMenu) {
+            printMenuOptions(menuOptions, selectedOption);
+        }
 
         // Choosing menu option
         if (pressedKey == '\r')
         {
             switch (selectedOption)
             {
-                // Start game
+                // Learn
             case 0:
                 system("CLS");
-                std::cout << "Start";
+                std::cout << "Learning mode selected." << std::endl;
+                // Implement learning mode functionality
                 break;
 
-                // Rules
+                // Start test
             case 1:
+            {
                 system("CLS");
-                std::cout << "Statistics";
+                std::vector<std::string> startTestOptions = { "9 Grade", "10 Grade", "Back" };
+                system("CLS");
+                if (subMenuStartTest(startTestOptions, menuOptions)) {
+                    displayMenu = true; // Display main menu after returning from submenu
+                }
+            }
+            break;
+
+            // Information
+            case 2:
+                system("CLS");
+                std::cout << "Displaying information..." << std::endl;
+                // Implement the information display functionality
                 break;
 
-                // Exit
-            case 2:
+                // Grades
+            case 3:
+            {
+                system("CLS");
+                std::vector<std::string> gradesOptions = { "Highest grade", "Students who passed the test", "Students who don't pass the test", "Back" };
+                system("CLS");
+                if (subMenuGrades(gradesOptions, menuOptions)) {
+                    displayMenu = true; // Display main menu after returning from submenu
+                }
+            }
+            break;
+
+            // Exit
+            case 4:
                 exitStatement = false;
                 system("CLS");
                 std::cout << "Press any key to exit";
@@ -152,6 +330,7 @@ void menu(std::vector<std::string> menuOptions)
         }
     }
 }
+
 int main()
 {
     Student student;
@@ -193,6 +372,6 @@ int main()
 
     system("CLS");
 
-    std::vector<std::string> menuOptions = { "Start Quiz", "Quiz Statistics", "Quit" };
+    std::vector<std::string> menuOptions = { "LEARN", "Start test", "Information", "Grades", "Exit" };
     menu(menuOptions);
 }
